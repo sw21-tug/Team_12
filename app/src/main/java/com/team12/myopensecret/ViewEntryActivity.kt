ff6c7b909a2480ee3725f6ae869b83224af5f643
Sanja Marinkovic <sanja.marinkovic@student.tugraz.at>
2021-06-05 14:12:14 +0200

    MOS-002 added changes in ViewEntryActivity.kt [ME, MS]

--- app/src/main/java/com/team12/myopensecret/ViewEntryActivity.kt
+++ app/src/main/java/com/team12/myopensecret/ViewEntryActivity.kt
@@ -1,15 +1,19 @@
 package com.team12.myopensecret
 
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
+private lateinit var entryList: LinearLayout
 
 class ViewEntryActivity: AppCompatActivity() {
 
@@ -35,7 +39,7 @@ class ViewEntryActivity: AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if (item.itemId == R.id.delete_entry) {
+        /* if (item.itemId == R.id. delete_entry) {
             val builder = AlertDialog.Builder(this)
             builder.setTitle(resources.getString(R.string.title_delete))
             builder.setMessage(resources.getString(R.string.description_delete))
@@ -50,8 +54,14 @@ class ViewEntryActivity: AppCompatActivity() {
             builder.show()
         }
 
+        return super.onOptionsItemSelected(item)*/
+        if(item.itemId == R.id.edit_entry){
+            val intent = Intent(this, EditEntryActivity::class.java)
+            startActivityForResult(intent, 0)
+        }
         return super.onOptionsItemSelected(item)
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
@@ -60,6 +70,25 @@ class ViewEntryActivity: AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.view_entry_action_bar, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun addToJournalsList(data: JournalDataEntry) {
+        var journalView: View = layoutInflater.inflate(R.layout.journal_entry, entryList, false)
+        journalView.findViewById<TextView>(R.id.journal_title).text = (data.title)
+        var desc = data.description
+        if (desc.length > 80)
+            desc = desc.substring(0, 80) + "..."
+        journalView.findViewById<TextView>(R.id.journal_description).text = desc
+        data.labels.forEach {
+            addLabelToGroup(it, journalView.findViewById<ChipGroup>(R.id.journal_chips))
+        }
+        journalView.setOnClickListener{
+            val intent = Intent(this, EditEntryActivity::class.java)
+            intent.putExtra("data", data)
+            startActivityForResult(intent, 1)
+
+        }
+        entryList.addView(journalView, 0)
     }
     private fun addLabelToGroup(label: LabelData, chipGroup: ChipGroup) {
         var labelChip = Chip(this)
