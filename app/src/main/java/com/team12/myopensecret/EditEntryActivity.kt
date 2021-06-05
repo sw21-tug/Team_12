5365f813e27c1fa6f79b849a70601c18fea5070a
Sanja Marinkovic <sanja.marinkovic@student.tugraz.at>
2021-06-05 15:00:13 +0200

    MOS-002 fields filled with existing data [ME,MS]

--- app/src/main/java/com/team12/myopensecret/EditEntryActivity.kt
+++ app/src/main/java/com/team12/myopensecret/EditEntryActivity.kt
@@ -1,40 +1,34 @@
 package com.team12.myopensecret
 
-import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
-import android.content.ContentValues
 import android.content.Intent
-import android.content.res.ColorStateList
-import android.database.Cursor
-import android.database.sqlite.SQLiteException
-import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class EditEntryActivity : AppCompatActivity() {
     private lateinit var model: JournalDataEntry
     private lateinit var titleField: EditText
     private lateinit var notesField: EditText
-    private lateinit var labelsGroup: ChipGroup
+    //private lateinit var labelsGroup: ChipGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SettingsActivity.loadLocale(this)
         setContentView(R.layout.activity_edit_entry)
-
+        titleField = findViewById(R.id.edit_title)
+        notesField = findViewById(R.id.notes_field)
+        val dataEntry = intent.extras?.getSerializable("data") as JournalDataEntry
+        titleField.setText(dataEntry.title)
+        notesField.setText(dataEntry.description)
         setTitle(R.string.edit_entry)
-
+        model = dataEntry
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,28 +40,29 @@ class EditEntryActivity : AppCompatActivity() {
 
         if (item.itemId == R.id.accept_entry) {
             startSuccessfulIntent()
-        } else if (item.itemId == R.id. delete_entry) {
+        } else if (item.itemId == R.id.delete_entry) {
             val builder = AlertDialog.Builder(this)
             builder.setTitle(resources.getString(R.string.title_delete))
             builder.setMessage(resources.getString(R.string.description_delete))
             builder.setPositiveButton(android.R.string.yes) { dialog, which ->
                 MainActivity.dataBase.deleteEmployee(model)
-                setResult(1)
-                finish()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivityForResult(intent, 1)
             }
             builder.setNegativeButton(android.R.string.no) { dialog, which ->
             }
 
             builder.show()
         }
+        //TODO: Else for cancel
 
         return super.onOptionsItemSelected(item)
     }
 
     private fun startSuccessfulIntent() {
-        var intent = Intent()
-        var title = titleField.text.toString().trim()
-        var notes = notesField.text.toString().trim()
+        val intent = Intent()
+        val title = titleField.text.toString().trim()
+        val notes = notesField.text.toString().trim()
         var hasError = false
         if (title.length < 5) {
             titleField.error = resources.getString(R.string.need5chars)
@@ -86,16 +81,20 @@ class EditEntryActivity : AppCompatActivity() {
             hasError = true
         }
         var hasSelectedChip = false
-        labelsGroup.checkedChipIds.forEach{
-            hasSelectedChip = true
-        }
-        if (hasError)
+        /*
+        FIXME: labelsGroup.checkedChipIds.forEach{
+             hasSelectedChip = true
+         }*/
+        if (hasError) {
             return
-        var selectedLabels = ArrayList<LabelData>()
-        labelsGroup.checkedChipIds.forEach{
-            selectedLabels.add(findViewById<Chip>(it).tag as LabelData)
         }
-        intent.putExtra("JOURNAL_ENTRY", JournalDataEntry(title,notes,selectedLabels, -1))
+
+        /* val selectedLabels = ArrayList<LabelData>()
+         labelsGroup.checkedChipIds.forEach{
+             selectedLabels.add(findViewById<Chip>(it).tag as LabelData)
+         } */
+        val selectedLabels = ArrayList<LabelData>()
+        intent.putExtra("JOURNAL_ENTRY", JournalDataEntry(title, notes, selectedLabels, model.dbId))
         setResult(1, intent)
         finish()
     }
